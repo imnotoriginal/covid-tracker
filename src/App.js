d188fe9f546f6c06e103ebec1cec51fa3937b52e
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-06-29 12:29:15 +0300

    add styles for App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { Cards, CountryPicker, Chart } from "./components";
-import "./App.module.css";
+import styles from "./App.module.css";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.container}>
         <Cards />
         <CountryPicker />
         <Chart />
