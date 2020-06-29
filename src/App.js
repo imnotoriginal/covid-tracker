7c769fabcbfeba06ab4cf4231eb4aedfc7dace15
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-06-29 12:27:45 +0300

    integrate components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import { Cards, Chart, CountryPicker } from "./components";
+import { Cards, CountryPicker, Chart } from "./components";
+import "./App.module.css";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
